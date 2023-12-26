94339a38eee81c0f26df834aa282c7c7143240c4
CVilla90 <carlosavillah90@gmail.com>
2023-12-25 19:58:02 -0600

    Improved experience images display and also project cards display, also moved the img to the top of the about me section only in the mobile version

--- CodeFolio/settings.py
+++ CodeFolio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codefolio--cavilla.repl.co', 'codefolio.cavilla.repl.co']
+ALLOWED_HOSTS = ['codefolio--cavilla.repl.co', 'codefolio.cavilla.repl.co', '127.0.0.1']
 
 
 # Application definition
